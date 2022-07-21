0052da75dbacc9e7110373f1114fdaed195487dc
rawunus <rawunus@gmail.com>
2022-07-21 11:13:39 -0400

    github ci yml upgraded command and added test case and fixed test fail error

--- server/src/routes/launches/launches.spec.js
+++ server/src/routes/launches/launches.spec.js
@@ -1,10 +1,12 @@
 const request = require("supertest");
 const app = require("../../app");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
+const { loadPlanetsData } = require("../../models/planet.model");
 
 describe("Launches apis", () => {
    beforeAll(async () => {
      await mongoConnect();
+     await loadPlanetsData();
    });
   
   afterAll(async () => {
